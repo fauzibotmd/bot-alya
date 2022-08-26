1ebd6a2fb12cc2fd8e52dbd2ca7924b23448c172
fauzibotmd <111993995+fauzibotmd@users.noreply.github.com>
2022-08-26 14:08:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Other
-global.owner = ['6285815975368']
-global.premium = ['6285815975368']
-global.ownername = 'Liya'
-global.botname = 'AliyaBot'
-global.packname = '© by AliyaBot'
+global.owner = ['19146271508']
+global.premium = ['19146271508']
+global.ownername = 'FauziGnteng'
+global.botname = 'FauziBotz-Md'
+global.packname = '© by fauzibotz'
 global.gc = 'https://bit.ly/3KMlmv2'
 global.linkyt = 'https://youtube.com/channel/UC9Si3U0o9dGS9MDfJR5iF6Q'
 global.linkgc = 'https://chat.whatsapp.com/DwP6uHYBWBc6TBSsNJrzwN'
 global.limitawal = '100'
-global.author = '@alya.xzy'
+global.author = 'Fauzi'
 global.sessionName = 'polo'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = 'terserah'
